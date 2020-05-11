2ffca2aa8ef3815bc4c2c9e57fc8d0a87d16adf0
jerryguo255 <jerryguo255@outlook.com>
2020-05-11 22:25:44 +1200

    lab04 finished

---
+++ L04/js/main.js
@@ -0,0 +1,40 @@
+var vue = new Vue({
+    el: '#app1',
+    data: {
+        people: null,
+        sid: 0,
+        idd: null
+
+    },
+    mounted: function () {
+        axios.get(
+            'http://jsonplaceholder.typicode.com/users')
+            .then(response =>
+                (this.people = response.data)
+            )
+            .catch((error) => {
+                this.people = "people not available";
+                console.log(error)
+            })
+    },
+    methods: {
+        personName: function (id) {
+            return id > 0 ? this.people[id - 1].name : "";
+        },
+        personEmail: function (id) {
+            return id > 0 ? this.people[id - 1].email : "";
+        },
+        personPhone: function (id) {
+            return id > 0 ? this.people[id - 1].phone : "";
+        },
+        personWebsite: function (id) {
+            return id > 0 ? this.people[id - 1].website : "";
+        },
+        personAddress: function (id) {
+            return id > 0 ? this.people[id - 1].address.city : "";
+        }
+
+    }
+
+
+})
